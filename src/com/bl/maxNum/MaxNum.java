dedc4c7f480bda6053f7083ab9d8a8249fd5d38b
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-10 13:42:50 +0530

    Added: UC2 Max Float

--- src/com/bl/maxNum/MaxNum.java
+++ src/com/bl/maxNum/MaxNum.java
@@ -2,14 +2,20 @@ package com.bl.maxNum;
 
 public class MaxNum {
 	
-	static Integer xInt = new Integer("700");
-	static Integer yInt = new Integer("2000");
+	static Integer xInt = new Integer("70");
+	static Integer yInt = new Integer("20");
 	static Integer zInt = new Integer("500");
 	static Integer max;
+	
+	static Float xF = new Float(120.5);
+	static Float yF = new Float(20.5);
+	static Float zF = new Float(5.5);
+	static Float max1;
 
 	
 	public static void main(String[] args) {
 		getMaxInt();
+		getMaxFloat();
 	}
 
 	
@@ -30,6 +36,26 @@ public class MaxNum {
 		else  {
 			max = xInt;
 			System.out.println("Maximum Integer is: " + max);
+		}
+		
+		}
+	
+	public static void getMaxFloat() {
+		max1 = xF;
+		int res = yF.compareTo(max1);
+		int res1 = zF.compareTo(max1);
+		int res2 = xF.compareTo(max1);
+		
+		if (res > 0) {
+			max1 = yF;
+			System.out.println("Maximum Float number is: " + max1);
+		} else if (res1 > 0) {
+			max1 = zF;
+			System.out.println("Maximum Float number is: " + max1);
+		}
+		else  {
+			max1 = xF;
+			System.out.println("Maximum Float number is: " + max1);
 		}
 		
 		}
